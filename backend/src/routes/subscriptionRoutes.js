71b75c88c91a3860614c035fdf29802466dc4e54
Gopulucky <gopuluckyhardik@gmail.com>
2026-02-27 21:29:51 +0530

    refactor: remove backend and move frontend to root for Vercel SPA hosting

--- backend/src/routes/subscriptionRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const subscriptionController = require('../controllers/subscriptionController');
-
-// POST /api/join
-router.post('/join', subscriptionController.subscribe);
-
-module.exports = router;
